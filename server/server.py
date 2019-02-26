82c42aad9def702c6aac4ff153884f6d8418c5bf
Anton Stagge <antonstagge95@gmail.com>
2019-02-26 17:25:28 +0100

    Added better exception handeling with custom APIError #3

--- server/server.py
+++ server/server.py
@@ -1,12 +1,11 @@
 import os
+from contextlib import contextmanager
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 import json
 from datetime import datetime
-
-
 
 # File paths
 root_path = os.path.realpath(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..'))
@@ -28,6 +27,7 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 #
 
+""" DATABASE MODEL """
 class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
@@ -72,72 +72,119 @@ class Planning_group(db.Model):
 
 admin.add_view(ModelView(Planning_group, db.session))
 
+""" API ENDPOINTS """
+class APIError(Exception):
+    def __init__(self, response, code):
+        super(APIError, self).__init__()
+        self.response = response
+        self.code = code
+
+@contextmanager
+def handle_exceptions():
+    try:
+        yield
+    except ValueError as e:
+        raise APIError(jsonify({'error': str(e)}), 400)
+    except KeyError as e:
+        raise APIError(jsonify({'error': "Missing parameter: " + str(e)}), 400)
+    except:
+        raise APIError(jsonify({'error': "Internal server error"}), 500)
+
+# Example payload:
+# {
+# 	"name":"test_user",
+# 	"auth_token":"test_token"
+# }
 @app.route('/api/createuser', methods=['POST'])
 @cross_origin() #dev only
 def create_user():
     try:
-        payload = request.json
-        name = payload['name']
-        auth_token = payload['auth_token']
-        if len(name) > 30:
-            raise ValueError("Name too long. Max 30 characters")
-        #TODO: input check auth token
-        new_user = User(name, auth_token)
-        db.session.add(new_user)
-        db.session.commit()
-        return (jsonify({'id': new_user.id}), 201)
-    except ValueError as e:
-        return jsonify({'error': str(e)}), 400
-    except KeyError as e:
-        return jsonify({'error': "Missing parameter: " + str(e)}), 400
-    except:
-        return jsonify({'error': "Internal server error"}), 500
+        with handle_exceptions():
+            payload = request.json
+            name = payload['name']
+            auth_token = payload['auth_token']
+            if len(name) > 30:
+                raise ValueError("Name too long. Max 30 characters")
+            #TODO: input check auth token
+            new_user = User(name, auth_token)
+            db.session.add(new_user)
+            db.session.commit()
+            return (jsonify({'id': new_user.id}), 201)
+    except APIError as e:
+        return e.response, e.code
 
-
+# Example payload
+# {
+# 	"name":"test_group",
+# 	"from_date":"2019-02-20",
+# 	"to_date":"2019-02-21",
+# 	"from_time":"10:00",
+# 	"to_time":"18:00",
+# 	"meeting_length":"01:00"
+# }
 @app.route('/api/creategroup', methods=['POST'])
 @cross_origin() #dev only
 def create_group():
     try:
-        payload = request.json
-        name = payload['name']
-        if len(name) > 30:
-            raise ValueError("Name too long. Max 30 characters")
-        new_group = Planning_group(
-            name,
-            datetime.strptime(payload['from_date'], '%Y-%m-%d').date(),
-            datetime.strptime(payload['to_date'], '%Y-%m-%d').date(),
-            datetime.strptime(payload['from_time'], '%H:%M').time(),
-            datetime.strptime(payload['to_time'], '%H:%M').time(),
-            datetime.strptime(payload['meeting_length'], '%H:%M').time(),
-            )
-        db.session.add(new_group)
-        db.session.commit()
-        return (jsonify({'id': new_group.id}), 201)
-    except ValueError as e:
-        return jsonify({'error': str(e)}), 400
-    except KeyError as e:
-        return jsonify({'error': "Missing parameter: " + str(e)}), 400
-    except:
-        return jsonify({'error': "Internal server error"}), 500
+        with handle_exceptions():
+            payload = request.json
+            name = payload['name']
+            if len(name) > 30:
+                raise ValueError("Name too long. Max 30 characters")
+            new_group = Planning_group(
+                name,
+                datetime.strptime(payload['from_date'], '%Y-%m-%d').date(),
+                datetime.strptime(payload['to_date'], '%Y-%m-%d').date(),
+                datetime.strptime(payload['from_time'], '%H:%M').time(),
+                datetime.strptime(payload['to_time'], '%H:%M').time(),
+                datetime.strptime(payload['meeting_length'], '%H:%M').time(),
+                )
+            db.session.add(new_group)
+            db.session.commit()
+            return (jsonify({'id': new_group.id}), 201)
+    except APIError as e:
+        return e.response, e.code
 
-@app.route('/api/addusertogroup', methods=['POST'])
+# Example payload
+# {
+# 	"user_id":2,
+# 	"group_id":3,
+# 	"command":"remove" | "add"
+# }
+@app.route('/api/updategroupusers', methods=['PUT'])
 @cross_origin() # dev only
 def add_user_to_group():
     try:
-        payload = request.json
-        user = User.query.filter_by(id= payload['user_id']).first()
-        if user is None:
-            raise ValueError("User not found")
-        group = Planning_group.query.filter_by(id= payload['group_id']).first()
-        if group is None:
-            raise ValueError("Group not found")
-        group.users.append(user)
-        db.session.commit()
-        return "", 200
-    except ValueError as e:
-        return jsonify({'error': str(e)}), 400
-    except:
-        return jsonify({'error': "Internal server error"}), 500
+        with handle_exceptions():
+            payload = request.json
+            user = User.query.filter_by(id= payload['user_id']).first()
+            if user is None:
+                raise ValueError("User not found")
+            group = Planning_group.query.filter_by(id= payload['group_id']).first()
+            if group is None:
+                raise ValueError("Group not found")
+            if payload['command'] == 'add':
+                group.users.append(user)
+            elif payload['command'] == 'remove':
+                group.users.remove(user)
+            else:
+                raise ValueError("Unknown command")
+            db.session.commit()
+            return jsonify({'group_id': group.id}), 200
+    except APIError as e:
+        return e.response, e.code
+
+# @app.route('/api/getusersfromgroup')
+# @cross_origin() # dev only
+# def get_users_from_group():
+#     try:
+#         with handle_exceptions():
+#             group_id = request.args['user_id']
+#             group = Planning_group.query.filter_by(id=group_id).first()
+#             if group is None:
+#                 raise ValueError("Group not found")
+#     except APIError as e:
+#         return e.response, e.code
 
 # Catch all routes and host index
 # So that we don't need browser router
