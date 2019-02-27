3e7b03e059a377b46d759865aea6f3190f762895
Marko Lazic <markolazic95@hotmail.com>
2019-02-27 16:30:53 +0100

    add simple google button and fetched the access token #4

--- client/src/containers/OwnerStart.tsx
+++ client/src/containers/OwnerStart.tsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../App.css';
+import GoogleLogin from 'react-google-login';
 
 class OwnerStart extends Component {
+
   render() {
     return (
       <div className="text-center">
@@ -11,6 +13,14 @@ class OwnerStart extends Component {
           <p>
             Edit <code>src/App.tsx</code> and save to relo
           </p>
+          <GoogleLogin
+            clientId="486151037791-q5avgjf6pc73d39v1uaalta9h3i0ha2d.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={'single_host_origin'}
+            scope={'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.events'}
+  />
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -23,6 +33,11 @@ class OwnerStart extends Component {
       </div>
     );
   }
+}
+
+const responseGoogle = (response: any) => {
+  console.log(response.Zi.access_token);
+
 }
 
 export default OwnerStart;
