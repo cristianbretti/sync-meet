c9e4fc137da100c0b882c81b523ac9be0ca17f65
Anton Stagge <antonstagge95@gmail.com>
2019-03-02 11:13:24 +0100

    Added tests for date and time input checking #7

--- server/tests.py
+++ server/tests.py
@@ -2,9 +2,15 @@ import os
 from flask_testing import TestCase
 from server import app, db, create_test_app, User, Planning_group
 import unittest
+from datetime import datetime
+from datetime import timedelta
 
-
-id_token = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImYyNGQ2YTE5MzA2NjljYjc1ZjE5NzBkOGI3NTRhYTE5M2YwZDkzMWYiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXpwIjoiNDg2MTUxMDM3NzkxLXE1YXZnamY2cGM3M2QzOXYxdWFhbHRhOWgzaTBoYTJkLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiNDg2MTUxMDM3NzkxLXE1YXZnamY2cGM3M2QzOXYxdWFhbHRhOWgzaTBoYTJkLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTE3ODMyMDczMTM1OTMyNjcwMTI4IiwiZW1haWwiOiJhbnRvbnN0YWdnZTk1QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdF9oYXNoIjoiM2tZZnR0WEJ6alJWaUpLdDRlVjV6USIsIm5hbWUiOiJBbnRvbiBTdGFnZ2UiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tLy03VFpGNmRXVkZoZy9BQUFBQUFBQUFBSS9BQUFBQUFBQUFBQS9BQ0hpM3JkRkFnd3FxeFV6TGhVLUQyQWhlNzdUVUc5NTRRL3M5Ni1jL3Bob3RvLmpwZyIsImdpdmVuX25hbWUiOiJBbnRvbiIsImZhbWlseV9uYW1lIjoiU3RhZ2dlIiwibG9jYWxlIjoic3YiLCJpYXQiOjE1NTE0NjE5MTYsImV4cCI6MTU1MTQ2NTUxNiwianRpIjoiYjYyMjVjMjM5OTc4N2I5YTJhNGE4ZDUzZTRiNTE3ZGFiMmE0NmE0ZiJ9.B9jUgY4xsHIFmxY3FMFelcWYkvNNUCySYG4TfO8V-C_XbgIblGv9IDdnQiPgJJTN0zWpDF9IXU2iewIV6ra0p1tZXM-rbEzpxtg_F603Bk2k-zHK92ipnLushup-TneY_t1v4IyuV-Tqo62ROgCoXmuAcknRqyRxZpviCeOzP7ORS8Qj7WmnFnyqK-VsfTVeXYNS0CLdUwy6-hcZhj_R8afQPuQObZTtZMUJCNugRi1Q9EF0p4iXgdlQfuTtBNvqeeZfkDoAuzC4gG4fhFuT6_l_kSPoKO8wECsvzcEFyN_J4yr4AhqC_5oloxhrvI86xZF5GBi6QRe1QEzDa8stew"
+id_token = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImNmMDIyYTQ5ZTk3ODYxNDhhZDBlMzc5Y2M4NTQ4NDRlMzZjM2VkYzEiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXpwIjoiNDg2MTUxMDM3NzkxLXE1YXZnamY2cGM3M2QzOXYxdWFhbHRhOWgzaTBoYTJkLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiNDg2MTUxMDM3NzkxLXE1YXZnamY2cGM3M2QzOXYxdWFhbHRhOWgzaTBoYTJkLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTE3ODMyMDczMTM1OTMyNjcwMTI4IiwiZW1haWwiOiJhbnRvbnN0YWdnZTk1QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdF9oYXNoIjoid0E4VEx4dUIyOHZ0VnlSS1lfYlJqUSIsIm5hbWUiOiJBbnRvbiBTdGFnZ2UiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tLy03VFpGNmRXVkZoZy9BQUFBQUFBQUFBSS9BQUFBQUFBQUFBQS9BQ0hpM3JkRkFnd3FxeFV6TGhVLUQyQWhlNzdUVUc5NTRRL3M5Ni1jL3Bob3RvLmpwZyIsImdpdmVuX25hbWUiOiJBbnRvbiIsImZhbWlseV9uYW1lIjoiU3RhZ2dlIiwibG9jYWxlIjoic3YiLCJpYXQiOjE1NTE1MTkzNDIsImV4cCI6MTU1MTUyMjk0MiwianRpIjoiYzM2YWJkZTcxOTQxZWZiN2Y1ZGI3NmFhYzc2ZTU0YjJiNDE2MTE1YiJ9.M5cLSqOrQ0bjx5d6skGliIXoxIQ9zvddsi44sKXPT8KJ9LoO0toUlXQ1oVzUvTKkRJzmNvDfecEZgcNn7RP4znP4bCfitKWy6MenEy3sURdrgxP4taVtGaQR8QXUFW9iTgeLAX66ZBBzGZvVkn1knG9TfWA2t2Y3cfqIljgErqUjUUHR0pxBsSAZN3CALDu_c3WsjExHSE50xxJjJLG7NYBZEuw4Huu37RDGFrx6iLg2RekiCqE0s6DpvOz7HGZBaiJLLL0qeGEjDRPu_Ce1dHInU7KaApp6U7uUzqIbpCfVo7RMGS6qMRpBunAIS1n5CW__Bh2Wx7k0tUCMVQwukA"
+from_date = str(datetime.now().date())
+to_date = str((datetime.now() + timedelta(days=1)).date())
+from_time = str(datetime.now().time())[:5]
+to_time = str((datetime.now() + timedelta(hours=2)).time())[:5]
+meeting_length = "01:00"
 
 class MyTest(TestCase):
     
@@ -28,11 +34,11 @@ class MyTest(TestCase):
     def test_creategroup_good(self):
         resp = self.client.post('/api/creategroup', json=dict(
             group_name="test_group",
-            from_date="2019-02-20",
-            to_date="2019-02-21",
-            from_time="10:00",
-            to_time="18:00",
-            meeting_length="01:00",
+            from_date=from_date,
+            to_date=to_date,
+            from_time=from_time,
+            to_time=to_time,
+            meeting_length=meeting_length,
             user_name="test_user",
             access_token="accessToken",
             id_token=id_token
@@ -51,11 +57,11 @@ class MyTest(TestCase):
     
     def test_creategroup_missing(self):
         resp = self.client.post('/api/creategroup', json=dict(
-            from_date="2019-02-20",
-            to_date="2019-02-21",
-            from_time="10:00",
-            to_time="18:00",
-            meeting_length="01:00",
+            from_date=from_date,
+            to_date=to_date,
+            from_time=from_time,
+            to_time=to_time,
+            meeting_length=meeting_length,
             user_name="test_user",
             access_token="accessToken",
             id_token=id_token
@@ -67,11 +73,11 @@ class MyTest(TestCase):
     def test_creategroup_invalid_date(self):
         resp = self.client.post('/api/creategroup', json=dict(
             group_name="test_group",
-            from_date="20d19-02-20",
-            to_date="2019-02-21",
-            from_time="10:00",
-            to_time="18:00",
-            meeting_length="01:00",
+            from_date=from_date,
+            to_date=to_date,
+            from_time="002:32",
+            to_time=to_time,
+            meeting_length=meeting_length,
             user_name="test_user",
             access_token="accessToken",
             id_token=id_token
@@ -83,11 +89,11 @@ class MyTest(TestCase):
     def test_creategroup_long_name(self):
         resp = self.client.post('/api/creategroup', json=dict(
             group_name="test_group",
-            from_date="2019-02-20",
-            to_date="2019-02-21",
-            from_time="10:00",
-            to_time="18:00",
-            meeting_length="01:00",
+            from_date=from_date,
+            to_date=to_date,
+            from_time=from_time,
+            to_time=to_time,
+            meeting_length=meeting_length,
             user_name="this is a vey long name what the hell is going on",
             access_token="accessToken",
             id_token=id_token
@@ -99,11 +105,11 @@ class MyTest(TestCase):
     def test_creategroup_invalid_id_token(self):
         resp = self.client.post('/api/creategroup', json=dict(
             group_name="test_group",
-            from_date="2019-02-20",
-            to_date="2019-02-21",
-            from_time="10:00",
-            to_time="18:00",
-            meeting_length="01:00",
+            from_date=from_date,
+            to_date=to_date,
+            from_time=from_time,
+            to_time=to_time,
+            meeting_length=meeting_length,
             user_name="test_user",
             access_token="accessToken",
             id_token="invalid"
@@ -111,6 +117,70 @@ class MyTest(TestCase):
         self.assert_400(resp)
         assert 'error' in resp.json
         assert 'Could not authenticate' in resp.json['error']
+    
+    def test_creategroup_in_past(self):
+        resp = self.client.post('/api/creategroup', json=dict(
+            group_name="test_group",
+            from_date="2018-01-01",
+            to_date=to_date,
+            from_time=from_time,
+            to_time=to_time,
+            meeting_length=meeting_length,
+            user_name="test_user",
+            access_token="accessToken",
+            id_token=id_token
+        ))
+        self.assert400(resp)
+        assert 'error' in resp.json
+        assert 'past' in resp.json['error']
+    
+    def test_creategroup_backwards_1(self):
+        resp = self.client.post('/api/creategroup', json=dict(
+            group_name="test_group",
+            from_date=from_date,
+            to_date=str((datetime.now() - timedelta(days=1)).date()),
+            from_time=from_time,
+            to_time=to_time,
+            meeting_length=meeting_length,
+            user_name="test_user",
+            access_token="accessToken",
+            id_token=id_token
+        ))
+        self.assert400(resp)
+        assert 'error' in resp.json
+        assert 'backwards' in resp.json['error']
+
+    def test_creategroup_backwards_2(self):
+        resp = self.client.post('/api/creategroup', json=dict(
+            group_name="test_group",
+            from_date=from_date,
+            to_date=to_date,
+            from_time="12:00",
+            to_time="11:59",
+            meeting_length=meeting_length,
+            user_name="test_user",
+            access_token="accessToken",
+            id_token=id_token
+        ))
+        self.assert400(resp)
+        assert 'error' in resp.json
+        assert 'backwards' in resp.json['error']
+    
+    def test_creategroup_more_than_year(self):
+        resp = self.client.post('/api/creategroup', json=dict(
+            group_name="test_group",
+            from_date=from_date,
+            to_date=str((datetime.now() + timedelta(days=361)).date()),
+            from_time=from_time,
+            to_time=to_time,
+            meeting_length=meeting_length,
+            user_name="test_user",
+            access_token="accessToken",
+            id_token=id_token
+        ))
+        self.assert400(resp)
+        assert 'error' in resp.json
+        assert 'year' in resp.json['error']
 
     """ TEST ADDUSER """ 
     def test_adduser_good(self):
