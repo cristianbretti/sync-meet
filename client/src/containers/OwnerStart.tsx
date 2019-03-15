482c5035079a8eb83015b6d187cd8ef08cec212b
Anton Stagge <antonstagge95@gmail.com>
2019-03-15 15:34:34 +0100

    Added logo to start screen #18

--- client/src/containers/OwnerStart.tsx
+++ client/src/containers/OwnerStart.tsx
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
-import '../App.css';
 import GoogleLogin from 'react-google-login';
 import api from '../api/api';
 import {ErrorResponse, CreateGroupBody, Time, MyDate, CreateGroupResponse, GetGroupCalendarResponse, EmptyResponse} from '../api/models';
+import AnimLogo from './logo/AnimLogo';
 
 class OwnerStart extends Component {
     render() {
         return (
         <div className="text-center">
-            <header className="App-header bg-blue">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-                Edit <code>src/App.tsx</code> and save to relod
-            </p>
+            <AnimLogo />
             <GoogleLogin
                 clientId="486151037791-q5avgjf6pc73d39v1uaalta9h3i0ha2d.apps.googleusercontent.com"
                 buttonText="Login"
@@ -22,15 +17,6 @@ class OwnerStart extends Component {
                 cookiePolicy={'single_host_origin'}
                 scope={'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.events'}
             />
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
-            </a>
-            </header>
         </div>
         );
     }
