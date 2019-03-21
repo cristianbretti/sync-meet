50d5aea3392aa24e77d8fd4c3611f43843b5dbce
Cristian Osorio Bretti <cristian.osoriobretti@gmail.com>
2019-03-21 19:59:25 +0100

    Increased width of Timebar (#24)

--- client/src/containers/Calendar.tsx
+++ client/src/containers/Calendar.tsx
@@ -97,12 +97,12 @@ class Calendar extends Component<RouteComponentProps<any>, CalendarState> {
           <div className="w-4/5 h-screen">  {/* The calendar goes in here */}
             <div className="flex">
 
-              <div className="w-1/40 h-screen border-t border-black"> {/* The timebar component goes in here */}
+              <div className="w-2/40 h-screen border-t border-black"> {/* The timebar component goes in here */}
                 <Timebar></Timebar>
               
               </div> 
 
-              <div className="w-39/40 overflow-x-auto flex flex-no-wrap">
+              <div className="w-38/40 overflow-x-auto flex flex-no-wrap">
                 {this.renderDays(tempState)}
               </div>
             </div>
