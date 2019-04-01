62bf47b5946be791df33430738a23c90b0269a58
Anton Stagge <antonstagge95@gmail.com>
2019-04-01 10:45:30 +0200

    Changed default src

--- client/src/components/logo/AnimLogo.tsx
+++ client/src/components/logo/AnimLogo.tsx
@@ -24,7 +24,7 @@ const AnimLogo: React.SFC<AnimLogoProps> = ({className}) => {
                 </div>
                 <div className="flex justify-center">
                     <div className="logo-letter"><img src={E} alt="E"/></div>
-                    <div className="logo-letter"><img src={E2} alt="left"/></div>
+                    <div className="logo-letter"><img src={E2} alt="E2"/></div>
                 </div>
             </div>
             <div><img src={right} alt="right"/></div>
