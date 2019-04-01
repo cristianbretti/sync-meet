dd95557ce46a7fd5479872ccb5e022a0b93230d7
Cristian Osorio Bretti <cristian.osoriobretti@gmail.com>
2019-04-01 13:08:03 +0200

    Added Timebar hour blocks (#24)

--- client/src/containers/Timebar.tsx
+++ client/src/containers/Timebar.tsx
@@ -7,15 +7,30 @@ interface TimebarProps {
 }
 
 const Timebar: React.FC<TimebarProps> = ({ from_time, to_time }) => {
+    let numberOfHoursBetweenStartEnd =
+        to_time.getHours() -
+        from_time.getHours() +
+        (to_time.getMinutes() > 0 ? 1 : 0)
     return (
-        <div className="h-screen text-center">
+        <div className="h-screen w-8">
             <div className="flex flex-col h-screen">
                 <div className="w-full bg-grey-light h-16 " />
 
-                <div className="w-full bg-grey flex-1 flex flex-col justify-between">
-                    <h6 className="p-2">{from_time.toString()}</h6>
-                    <h6 className="p-2">{to_time.toString()}</h6>
-                </div>
+                {Array.from(Array(numberOfHoursBetweenStartEnd)).map(
+                    (v, idx) => {
+                        const hour = from_time.getHours() + idx
+                        return (
+                            <div key={idx} className="flex flex-col flex-1">
+                                <div className="flex-1 border-b border-dotted text-2xs">
+                                    {hour < 10
+                                        ? '0' + hour + ':00'
+                                        : hour + ':00'}
+                                </div>
+                                <div className="flex-1 border-b" />
+                            </div>
+                        )
+                    }
+                )}
             </div>
         </div>
     )
